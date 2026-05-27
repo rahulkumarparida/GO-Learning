6a16ff24bf126e952cb777eb056fdac7d8baf456
rahulkumarparida <rahulkumarparida52@gmail.com>
2026-05-27 09:46:35 +0530

    Wrote programs on Bubble sort , stack Implementation and queue ImlementatioN

--- Practice/practice.go
+++ Practice/practice.go
@@ -14,19 +14,25 @@ func main() {
 	// findLargest()
 	// totalEvenNum()
 	// fizzBuzz()	
+	// bubbleSort()
+	// stackImplementation()
+	// queueImplementation()
+	validParanthesess()
+
 	// fmt.Print("\nEnter a Sring: ")
 	// scanner := bufio.NewScanner(os.Stdin)
 	// var inp string
 	// if scanner.Scan() {
-		// 	inp = scanner.Text()
-		// }
+	// 		inp = scanner.Text()
+	// 	}
 		
 		// reverseString(inp)
 		// checkPalindrome(inp)
 		// linearSearch(inp)
 		// frequncyCounter(inp)
 		// removeDuplicates(inp)
-		twoSum()	
+		// twoSum()	
+		// wordCounter(inp)
 }
 
 func sumOfArr() {
@@ -259,3 +265,273 @@ func twoSum() {
 }
 
 
+func wordCounter(inp string){
+
+	arrOfWords := strings.Split(inp, " ")
+	counter := len(arrOfWords)	
+
+	fmt.Println("Sentence: ", counter)
+
+
+}
+
+
+
+func convToInt(val1 string) int{
+	val,err := strconv.Atoi(val1)
+	if err != nil {
+		panic(err)
+	}
+	return  val
+}
+
+func convStrToIntArr(inp []string) []int{
+	arrOfNumbers := []int{}
+	for _,val := range inp{
+		val  := convToInt(val)
+		
+		arrOfNumbers = append(arrOfNumbers, val)
+	}
+	return  arrOfNumbers
+}
+
+func bubbleSort(){
+	fmt.Print("\nEnter a series of numbers: ")
+	scanner := bufio.NewScanner(os.Stdin)
+	var inp string
+	if scanner.Scan() {
+			inp = scanner.Text()
+	}
+	arrOfStrNumbers := strings.Split(inp, " ")
+	arrOfNumbers := convStrToIntArr(arrOfStrNumbers)
+	
+	
+	for i:=0;i < len(arrOfNumbers) ;i++{
+		for j := 0; j < len(arrOfNumbers); j++ {
+			var presentElem = arrOfNumbers[i]
+
+			if arrOfNumbers[i] < arrOfNumbers[j] {
+
+				arrOfNumbers[i] = arrOfNumbers[j]
+				arrOfNumbers[j] = presentElem	
+				continue
+			}
+			
+		}
+	}
+	fmt.Println("Sorted: ",arrOfNumbers)
+
+
+}
+
+
+func push(arr []int) []int{
+	fmt.Print("\nEnter a int: ")
+	scanner := bufio.NewScanner(os.Stdin)
+	var inp string
+	if scanner.Scan() {
+			inp = scanner.Text()
+		}
+	num := convToInt(inp)
+
+	arr = append(arr, num)
+	fmt.Println("Appended: ", num)
+	fmt.Println("Slice: ",arr)
+	return  arr
+}
+
+func pop(arr []int) []int{
+	lastVal := arr[len(arr)-1]
+	arr = arr[:(len(arr)-1)]
+
+	fmt.Println("Popped: ",lastVal)
+	fmt.Println("Slice: ",arr)
+	return  arr 
+
+}
+
+func peek(arr []int) {
+
+	fmt.Println("Top Element: ", arr[len(arr)-1])
+	fmt.Println("Slice: ",arr)
+
+}
+
+func displayStack(arr []int){
+
+	for i:=len(arr)-1;i>0;i-- {
+		fmt.Println("|         ",arr[i],"           |")
+		fmt.Println("|---------------------------|")
+	}
+
+}
+
+func stackImplementation(){
+	fmt.Println("Enter a series of numbers: ")
+	scanner := bufio.NewScanner(os.Stdin)
+	var inp string
+	if scanner.Scan() {
+			inp = scanner.Text()
+	}
+	arrOfStrNumbers := strings.Split(inp, " ")
+	arrOfNumbers := convStrToIntArr(arrOfStrNumbers)
+
+	for {
+		fmt.Println("1.Push an element")	
+		fmt.Println("2.Pop an element")	
+		fmt.Println("3.Peek an element")	
+		fmt.Println("4.Display the Stack")	
+		
+		fmt.Print("\nEnter a option: ")	
+		scanner := bufio.NewScanner(os.Stdin)
+		var operation string
+		if scanner.Scan() {
+				operation = scanner.Text()
+		}
+
+
+		switch operation {
+		case "1":
+			arrOfNumbers = push(arrOfNumbers)
+		case "2":
+			arrOfNumbers = pop(arrOfNumbers)
+		case "3":
+			peek(arrOfNumbers)
+		case "4":
+			displayStack(arrOfNumbers)
+		default:
+			os.Exit(1)
+			
+		}
+	}
+}
+
+
+
+
+func enqueue(arr []int) []int{
+	fmt.Print("\nEnter a int: ")
+	scanner := bufio.NewScanner(os.Stdin)
+	var inp string
+	if scanner.Scan() {
+			inp = scanner.Text()
+		}
+	num := convToInt(inp)
+
+	arr = append(arr, num)
+	fmt.Println("Enqueued: ", num)
+	fmt.Println("Slice: ",arr)
+	return  arr
+
+}
+
+func dequeue(arr []int) []int{
+
+		lastVal := arr[len(arr)-1]
+	arr = arr[1:]
+
+	fmt.Println("Dequeued: ",lastVal)
+	fmt.Println("Slice: ",arr)
+	return  arr 
+
+}
+
+func displayQueue(arr []int){
+	
+	fmt.Println("|______________________________________________________|")
+	for _,val := range arr{
+		fmt.Print(" < ",val)
+	}
+	fmt.Println("\n|______________________________________________________|")
+
+
+}
+
+
+func queueImplementation(){
+	fmt.Println("Enter a series of numbers: ")
+	scanner := bufio.NewScanner(os.Stdin)
+	var inp string
+	if scanner.Scan() {
+			inp = scanner.Text()
+	}
+	arrOfStrNumbers := strings.Split(inp, " ")
+	arrOfNumbers := convStrToIntArr(arrOfStrNumbers)
+
+	for {
+		fmt.Println("1.Add an element from the last")	
+		fmt.Println("2.Delete an element from the top")	
+		fmt.Println("3.Display the queue")	
+		
+		fmt.Print("\nEnter a option: ")	
+		scanner := bufio.NewScanner(os.Stdin)
+		var operation string
+		if scanner.Scan() {
+				operation = scanner.Text()
+		}
+
+
+		switch operation {
+		case "1":
+			arrOfNumbers = enqueue(arrOfNumbers)
+		case "2":
+			arrOfNumbers = dequeue(arrOfNumbers)
+		case "3":
+			displayQueue(arrOfNumbers)
+		default:
+			os.Exit(1)
+			
+		}
+	}
+
+}
+
+
+
+
+func checkParantheses(left string,right string) bool{
+	
+	if left == "(" && right == ")" {
+		return true
+		
+	}else if left == "[" && right == "]" {
+		return true
+	}else if left == "{" && right == "}" {
+		return true
+	}else{
+		return false
+	}
+}
+
+// Work in progresss THis one
+
+func validParanthesess()  {
+	fmt.Println("Enter a series of brackets(no space): ")
+	scanner := bufio.NewScanner(os.Stdin)
+	var inp string
+	if scanner.Scan() {
+			inp = scanner.Text()
+	}
+	arrOfStrBrackets := strings.Split(inp, "")
+	if len(arrOfStrBrackets)%2 != 0 {
+		panic("All the brackets are not correctly placed")
+	}
+
+	stackElement := []string{}
+
+	for _ , val := range arrOfStrBrackets {
+
+		stackElement = append(stackElement, val)
+		
+		if checkParantheses(val , stackElement[len(stackElement)-1]){
+			arrOfStrBrackets = arrOfStrBrackets[:len(arrOfStrBrackets)-1]
+			continue
+		}
+
+
+
+	}
+
+
+	
+}
